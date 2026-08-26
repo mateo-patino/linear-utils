2892e6301e5664cf1a59d4917a09e4955ee1b749
Mateo Patino <mateopatinodev@gmail.com>
2026-08-26 16:14:16 -0500

    linalg: add matrix view defintion

---
+++ src/linalg/view.h
@@ -0,0 +1,24 @@
+#ifndef VIEW_H
+#define VIEW_H
+
+#include "linalg/scalar.h"
+
+#include <stdlib.h>
+
+
+/*
+* The core matrix structure in this library is the  matrix view
+* struct `matrixv_t`. It encompasses all the information required
+* to read data from and write data to a matrix's `data` array.
+*/
+typedef struct {
+    scalar *data;
+    size_t ncol;
+    size_t nrow;
+    size_t column_stride;
+    size_t row_stride;
+} matrixv_t;
+
+
+
+#endif
