75c5c14ecb5f1343879633b44f4dc765e7611648
Mateo Patino <mateopatinodev@gmail.com>
2026-09-06 13:49:04 -0400

    linalg: add 2-strided matrix multiplication and document restrict requirement

--- src/linalg/arithmetic.c
+++ src/linalg/arithmetic.c
@@ -89,39 +89,68 @@ int matrix_sub(matrixv_t *C, const matrixv_t *A, const matrixv_t *B) {
 
 
 /*
-* Matrix multiplication (in the order AB)
+* Matrix multiplication (in the order AB).
+*
+* Pointers C, A, and B and their respective `data` pointers must be unique to prevent 
+* undefined behavior (updating an entry and then reading it when the original value was 
+* expected, specifically ). Hence, usage like A = AB or A = AA is not allowed.
 */
-int matrix_mul(matrixv_t *C, const matrixv_t *A, const matrixv_t *B) {
+int matrix_mul(matrixv_t *restrict C, const matrixv_t *restrict A, const matrixv_t *restrict B) {
     if (!C || !A || !B || A->ncol != B->nrow || C->nrow != A->nrow
         || C->ncol != B->ncol) {
         return -1;
     }
 
-    /* Initialize the output matrix's entries to zero */
-    #pragma omp simd
-    for (size_t i = 0, nentry = C->nrow * C->ncol; i < nentry; i++) {
-        C->data[i] = 0;
-    }
+    size_t nrow = C->nrow, ncol = C->ncol, nentry = ncol * nrow;
+    size_t C_rs = C->row_stride, C_cs = C->column_stride;
+    size_t A_rs = A->row_stride, A_cs = A->column_stride;
+    size_t B_rs = B->row_stride, B_cs = B->column_stride;
 
-    size_t nrow = C->nrow, ncol = C->ncol;
-    size_t C_rs = C->row_stride, A_rs = A->row_stride, B_rs = B->row_stride;
-    scalar *C_data = C->data, *A_data = A->data, *B_data = B->data;
-
-    /* A->ncol == B->nrow, this is the matching dimension between A and B */
+    /* All matrix entries MUST unique */
+    scalar *restrict C_data = C->data;
+    const scalar *restrict A_data = A->data, *restrict B_data = B->data;
     size_t shared_dimension = A->ncol; 
 
+    /* Contiguous in memory (non-strided) view */
+    if ((C_rs == C->ncol && C_cs == 1) &&
+        (A_rs == A->ncol && A_cs == 1) &&
+        (B_rs == B->ncol && B_cs == 1)) {
+           
+        /* Initialize the output matrix's entries to zero */
+        #pragma omp simd
+        for (size_t i = 0; i < nentry; i++) {
+            C->data[i] = 0;
+        }
+
+        /* A->ncol == B->nrow, this is the matching dimension between A and B */
+        for (size_t i = 0; i < nrow; i++) {
+            for (size_t j = 0; j < ncol; j++) {
+                for (size_t k = 0; k < shared_dimension; k++) {
+                    C_data[i * C_rs + j] += A_data[i * A_rs + k] * B_data[k * B_rs + j];
+                }
+            }
+        }
+        return 0;
+    }
+
+    /* Zero out the entries */
+    for (size_t i = 0; i < nrow; i++) {
+        #pragma omp simd
+        for (size_t j = 0; j < ncol; j++) {
+            C_data[i * C_rs + j * C_cs] = 0;
+        }
+    }
+
+    /* NEEDSWORK: can be optimized by reordering loop */
     for (size_t i = 0; i < nrow; i++) {
         for (size_t j = 0; j < ncol; j++) {
             for (size_t k = 0; k < shared_dimension; k++) {
-                C_data[i * C_rs + j] += A_data[i * A_rs + k] * B_data[k * B_rs + j];
+                C_data[i * C_rs + j * C_cs] += A_data[i * A_rs + k * A_cs] * B_data[k * B_rs + j * B_cs];
             }
         }
     }
-    return 0;
 
-    /*
-    * NEEDSWORK: we need the 2D strided version of these functions 
-    */
+    return 0;
 }
 
 
--- src/linalg/arithmetic.h
+++ src/linalg/arithmetic.h
@@ -11,10 +11,20 @@
 * and -1 upon failure.
 */
 
+
+/*
+* Addition and subtraction allow pointer aliasing because A[i] = A[i] +/- A[i]
+* and the like is well-defined.
+*/
 int matrix_add(matrixv_t *C, const matrixv_t *A, const matrixv_t *B);
 int matrix_sub(matrixv_t *C, const matrixv_t *A, const matrixv_t *B);
-int matrix_mul(matrixv_t *C, const matrixv_t *A, const matrixv_t *B);
 
-
+/*
+* Matrix multiplication does not allow pointer aliasing. 
+* Pointers C, A, and B and their respective `data` pointers must be unique to prevent 
+* undefined behavior (updating an entry and then reading it when the original value was 
+* expected, specifically). Hence, usage like A = AB or A = AA is not allowed.
+*/
+int matrix_mul(matrixv_t *restrict C, const matrixv_t *restrict A, const matrixv_t *restrict B);
 
 #endif
