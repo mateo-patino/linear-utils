d6f1b2f4d1226ffcf74051ec13b0b0aa4ea28deb
Mateo Patino <mateopatinodev@gmail.com>
2026-09-09 00:58:00 -0400

    linalg: add convert to upper triangular form via Gaussian eliminiation

--- src/linalg/row_operations.c
+++ src/linalg/row_operations.c
@@ -1,5 +1,8 @@
 #include "linalg/row_operations.h"
 
+#include <math.h>
+
+
 
 int swap_rows(size_t i, size_t j, matrixv_t *A) {
     if (!A || i >= A->nrow || j >= A->nrow) {
@@ -55,6 +58,61 @@ int add_row_multiple(size_t i, scalar factor, size_t j, matrixv_t *A) {
         data[i * rs + c * cs] += factor * data[j * rs + c * cs];
     }
 
+    return 0;
+}
+
+
+int to_upper_triangular(matrixv_t *A) {
+    if (!A || A->nrow != A->ncol) {
+        return -1;
+    }
+
+    size_t ncol = A->ncol, nrow = A->nrow, rs = A->row_stride, cs = A->column_stride;
+    scalar *data = A->data;
+
+    /* Iterate through the columns in the matrix */
+    for (size_t j = 0; j < ncol; j++) {
+        
+        /* 
+        * Find the largest entry in the current column and make it the pivot by
+        * making its row the pivot row. This helps us avoid multiplying by large
+        * values which can amplify numerical errors.
+        */
+        size_t prow = j;
+        for (size_t i = j + 1; i < nrow; i++) {
+            if (fabs(data[i * rs + j * cs]) > fabs(data[prow * rs + j * cs])) {
+                prow = i;
+            }
+        }
+
+        /* Move the pivot row to the j-th row index. The pivot row is then at index j */
+        if (prow != j) {
+            swap_rows(prow, j, A);
+        }
+        scalar pivot_entry = data[j * rs + j * cs];
+
+        /* 
+        * Check if singular matrix (i.e. all entries in the rows >= j are 0) 
+        * NEEDSWORK: Note we check exactly for 0 and not for proximity. Could we 
+        * want to check for proximity instead?
+        */
+        if (pivot_entry == 0) {
+            return 1;
+        }
+
+        /*
+        * Cancel the entries below the pivot entry by adding a multiple of the pivot
+        * row to the row of each entry.
+        */
+        scalar factor;
+        for (size_t i = j + 1; i < nrow; i++) {
+            factor = data[i * rs + j * cs] / pivot_entry;
+            add_row_multiple(i, -1 * factor, j, A);
+        }
+        
+        /* Repeat with the next column, starting one row below this time (note the i = j + 1) */ 
+    }
+    
     return 0;
 }
 
--- src/linalg/row_operations.h
+++ src/linalg/row_operations.h
@@ -14,6 +14,18 @@ int swap_rows(size_t i, size_t j, matrixv_t *A);
 /* Set row_i <- row_i + factor * row_j */
 int add_row_multiple(size_t i, scalar factor, size_t j, matrixv_t *A);
 
+
+/*
+* Row-reduces `A` to an upper triangular form. `A` must be a square matrix.
+* Note that the row-reduction is done in place, so `A` will be directly modified.
+*
+* It returns -1 upon failure, 0 upon success, and 1 if the matrix is singular and 
+* cannot be put into upper triangular form.
+
+* This functions uses Gaussian elimination with partial pivoting. 
+*/
+int to_upper_triangular(matrixv_t *A);
+
 #endif
 
 
