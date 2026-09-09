43cc8e7cdcd1acca55009dbd4da14c6567decdf4
Mateo Patino <mateopatinodev@gmail.com>
2026-09-08 23:44:05 -0400

    linalg: add row swapping

---
+++ src/linalg/row_operations.c
@@ -0,0 +1,31 @@
+#include "linalg/row_operations.h"
+
+
+int swap_rows(size_t i, size_t j, matrixv_t *A) {
+    if (!A || i >= A->nrow || j >= A->nrow) {
+        return -1;
+    }
+
+    /* No op if swapping the same row with itself */
+    if (i == j) {
+        return 0;
+    }
+
+    /* NEEDSWORK: could likely do a non-strided path? Could in theory be slightly faster */
+    
+    size_t ncol = A->ncol, rs = A->row_stride, cs = A->column_stride;
+    size_t i_idx, j_idx;
+    scalar *data = A->data;
+    scalar tmp;
+
+    for (size_t c = 0; c < ncol; c++) {
+        i_idx = i * rs + c * cs;
+        j_idx = j * rs + c * cs;
+
+        tmp = data[i_idx];
+        data[i_idx] = data[j_idx];
+        data[j_idx] = tmp;
+    }
+
+    return 0;     
+}
---
+++ src/linalg/row_operations.h
@@ -0,0 +1,19 @@
+#ifndef ROW_OPERATIONS_H
+#define ROW_OPERATIONS_H
+
+#include "linalg/view.h"
+#include "linalg/scalar.h"
+
+
+/* 
+* Elementary row operations. These operations are done in-place (i.e. they 
+* modify matrix A). i and j are zero-indexed.
+*/
+int swap_rows(size_t i, size_t j, matrixv_t *A);
+
+/* Set row_i <- row_i + factor * row_j */
+int add_rows(size_t i, scalar factor, size_t j, matrixv_t *A);
+
+#endif
+
+
