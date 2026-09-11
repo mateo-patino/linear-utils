6b100369ae9996a20187fa0c8ffe6eb80810ef79
Mateo Patino <mateopatinodev@gmail.com>
2026-09-11 14:26:07 -0400

    linalg: add determinant function

---
+++ src/linalg/unary_operations.c
@@ -0,0 +1,34 @@
+#include "linalg/unary_operations.h"
+#include "linalg/row_operations.h"
+
+
+int matrix_det(scalar *out, matrixv_t *A) {
+    if (!out || !A || A->nrow != A->ncol) {
+        return -1;
+    }
+
+    /* Put into upper triangular form */
+    int row_swaps = 0;
+    int ok;
+    if ((ok = to_upper_triangular(A, &row_swaps)) == -1) {
+        return -1;
+    }
+    else if (ok == 1) {
+        return 1;
+    }
+
+    /* Adjust sign for row swaps */
+    scalar factor = ((row_swaps & 1) == 0) ? 1 : -1;
+
+    /* Multiply entries along the diagonal */
+    scalar diagonal_product = 1;
+    size_t nrow = A->nrow, rs = A->row_stride, cs = A->column_stride;
+    const scalar *data = A->data;
+    for (size_t i = 0; i < nrow; i++) {
+        diagonal_product *= data[i * rs + i * cs];
+    }
+
+    *out = factor * diagonal_product;
+
+    return 0;
+}
---
+++ src/linalg/unary_operations.h
@@ -0,0 +1,31 @@
+#ifndef UNARY_OPERATIONS_H
+#define UNARY_OPERATIONS_H
+
+/*****************************************************************
+* This file provides public functions for performing
+* unary operations, such as computing determinants, converting
+* to RREF, inversion, etc.
+*****************************************************************/
+
+
+#include "linalg/view.h"
+#include "linalg/scalar.h"
+
+
+/*
+* Compute the determinant of `A`. 
+*
+* The result is written to `out` and 0 is returned upon success.
+* If the determinant of `A` cannot be computed, 1 is returned if `A`
+* is a singular matrix and -1 is returned if `A` is not square or 
+* or another issue occurs. The function will reject any non-square
+* matrix.
+*
+* Note: The entries of `A` will be modified by this function. Internally,
+* row-reduction is used to put `A` into upper-triangular form, so provide
+* a deep copy of `A` if you wish to preserve the original matrix.
+*/
+int matrix_det(scalar *out, matrixv_t *A);
+ 
+
+#endif
