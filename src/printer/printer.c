4fc27c5ce0dfdb8ba21b4cc6e6463d5a1f099b76
Mateo Patino <mateopatinodev@gmail.com>
2026-09-25 21:37:51 -0400

    printer: add function to find max width

--- src/printer/printer.c
+++ src/printer/printer.c
@@ -7,12 +7,16 @@
 
 static bool print_scalar(FILE *stream, const scalar_t *scalar, int precision, bool add_newline);
 static bool has_no_fractional_part(const scalar_t *scalar);
-
-static bool has_no_fractional_part(const scalar_t *scalar) {
-    return scalar && isfinite(*scalar) && *scalar == trunc(*scalar);
-}
+static size_t get_max_column_width(const matrix_t *matrix);
 
 
+/*
+* Prints a the value pointed at by `scalar` to `stream`. 
+*
+* It prints a newline character if `add_newline` is true, and the scalar is printed 
+* with `precision` decimal points if its fractional part is not zero. If the fractional
+* part is zero, the value is printed as an integer.
+*/
 static bool print_scalar(FILE *stream, const scalar_t *scalar, int precision, bool add_newline) {
     if (!stream || !scalar) {
         return false;
@@ -33,6 +37,52 @@ static bool print_scalar(FILE *stream, const scalar_t *scalar, int precision, bo
 }
 
 
+/*
+* Returns true if the value at `scalar` has a no fractional part
+*/
+static bool has_no_fractional_part(const scalar_t *scalar) {
+    return scalar && isfinite(*scalar) && *scalar == trunc(*scalar);
+}
+
+
+/*
+* Computes the maximum column width required to display all 
+* entries in a matrix.
+*
+* It returns the maximum column width found upon success and SIZE__MAX 
+* if an error occurs.
+*/
+static size_t get_max_column_width(const matrix_t *matrix) {
+    if (!matrix || !matrix->data) {
+        return 0;
+    }
+
+    const scalar_t *data = matrix->data;
+    unsigned int nentry = matrix->nrow * matrix->ncol;
+
+    size_t max_len = 0;
+    int len = 0;
+    for (unsigned int i = 0; i < nentry; i++) {
+
+        if (has_no_fractional_part(data + i)) {
+            len = snprintf(NULL, 0, "%.0f", data[i]); 
+        }
+        else {
+            len = snprintf(NULL, 0, PRISCALAR, SCALAR_PRECISION, data[i]);
+        }
+
+        if (len < 0) {
+            set_error("Could not get maximum column width.");
+            return SIZE_MAX;
+        }
+
+        max_len = (size_t)len > max_len ? (size_t)len : max_len;
+    }
+
+    return max_len; 
+}
+
+
 bool pretty_print(const printout_t *pout) {
     if (!pout) {
         return false;
@@ -47,7 +97,7 @@ bool pretty_print(const printout_t *pout) {
     }
 
     if (!ok) {
-        set_error("Could not print object.");
+        set_error("Could not print output.");
     }
 
     return ok;
@@ -57,9 +107,16 @@ bool pretty_print(const printout_t *pout) {
 bool pretty_print_matrix(const matrix_t *matrix) {
     if (!matrix) {
         return false;
+    } 
+
+    size_t col_width = get_max_column_width(matrix);
+    if (col_width == SIZE_MAX) {
+        set_error("Could not print output.");
+        return false;
     }
 
-    /* TODO */
+
+
     return true;
 }
 
